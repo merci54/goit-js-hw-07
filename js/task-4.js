88a596fe2ad17405c1e19a1ca1f4fe4ebbada90f
Yaroslav <yaroslavlit@gmail.com>
2025-05-20 11:01:16 +0200

    task4 full done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const formEL = document.querySelector('.login-form');
 
+const handlerSubmit = event => {
+    event.preventDefault();
+
+    const form = event.target;
+    const emailInput = event.target.elements.email.value.trim();
+    const passwordInput = event.target.elements.password.value.trim();
+
+    const info = {
+        email: emailInput,
+        password: passwordInput,
+    }
+
+    emailInput === '' || passwordInput === '' ? alert('All form fields must be filled in') : console.log(info);
+    form.reset();
+    
+}
+formEL.addEventListener('submit', handlerSubmit);
