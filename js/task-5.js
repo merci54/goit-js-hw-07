d260969b49c8e03ce42596a1ada4210c7bd14e6a
Yaroslav <yaroslavlit@gmail.com>
2025-05-20 11:25:53 +0200

    task5 full done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+function handlerClick(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor()
+
+}
+
+buttonEl.addEventListener('click', handlerClick)
